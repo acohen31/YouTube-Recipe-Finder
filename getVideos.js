5870ae4b69e3aefb83d06b2c84198e4e78cd78cd
Aviv Cohen <avivcohen31@gmail.com>
2023-10-06 22:57:57 -0400

    Add feature to getVideos(), stores titles and descriptions in map

--- getVideos.js
+++ getVideos.js
@@ -1,6 +1,6 @@
 
-const URL = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB30w61K2yt8xwZrYKWeBOIT3yVOJIseMI&q=pizza-recipe&type=video&part=snippet&maxResults=25&order=viewCount&topicID=cooking';
-
+const URL = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB30w61K2yt8xwZrYKWeBOIT3yVOJIseMI&q=pizza recipe&type=video&part=snippet&maxResults=2&order=viewCount&topicID=cooking';
+const API_KEY = 'AIzaSyB30w61K2yt8xwZrYKWeBOIT3yVOJIseMI'
 // const video = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={VIDEO_ID}&key=AIzaSyB30w61K2yt8xwZrYKWeBOIT3yVOJIseMI&q';
 
 function getVideos() {
@@ -14,23 +14,26 @@ function getVideos() {
         const data = await response.json();
         const items = data.items;
         
-        const VIDEO_ID = (items[0].id.videoId);
-        const VIDEO = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${VIDEO_ID}&key=AIzaSyB30w61K2yt8xwZrYKWeBOIT3yVOJIseMI&q`;
-        const videoResponse = await fetch(VIDEO);
-        if (videoResponse.status !== 200) {
-            console.log(Error("Something is wrong!"));
-        }
+        videoDict = new Map();
 
-        const videoData = await videoResponse.json();
-        console.log(videoData.items[0].snippet.description);
-        
+        for (const item of items) {
+            const VIDEO_ID = item.id.videoId;
+            const VIDEO = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${VIDEO_ID}&key=${API_KEY}`;
+            const videoResponse = await fetch(VIDEO);
+            const videoData = await videoResponse.json();
+            const description = videoData.items[0].snippet.description;
+            videoDict.set(item.snippet.title, description);
+        };
+
+        videoDict.forEach((description, title) => {
+            console.log(title);
+            console.log(description)
+        });
+
     }
 
-    func();
-
+    func()
     
 }
-
-
 
 getVideos();
